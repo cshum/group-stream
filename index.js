a1a826c5e92d50379b2e20ff56a7bcb34e2f7c73
Finn Pauls <derfinn@gmail.com>
2018-02-13 01:56:53 +0100

    Use new stream.Transform syntax instead of through2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var through = require('through2')
+var Transform = require('stream').Transform
 
 function defaultKey (val) {
   return val.key || val
@@ -9,17 +9,21 @@ module.exports = function group (toKey) {
     toKey = defaultKey
   }
   var curr, value
-  return through.obj(function (data, enc, cb) {
-    var next = toKey(data)
-    if (curr !== next) {
+  return new Transform({
+    objectMode: true,
+    transform: function (data, enc, cb) {
+      var next = toKey(data)
+      if (curr !== next) {
+        if (value) this.push({ key: curr, value: value })
+        value = []
+        curr = next
+      }
+      value.push(data)
+      cb()
+    },
+    flush: function (cb) {
       if (value) this.push({ key: curr, value: value })
-      value = []
-      curr = next
+      cb()
     }
-    value.push(data)
-    cb()
-  }, function (cb) {
-    if (value) this.push({ key: curr, value: value })
-    cb()
   })
 }
